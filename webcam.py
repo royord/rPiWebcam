0d24551817c28fed1d872de46c23c222c20190ac
royord <royord@royord.com>
2025-11-03 21:42:42 -0800

    update to set the RTC time

--- webcam.py
+++ webcam.py
@@ -293,6 +293,42 @@ class webcam:
 
         return quality_num
 
+    def update_time(self):
+        """
+        Update the time from the internet and then set the hardware
+        clock.
+        """
+        loop_time_set = 10
+        is_set = False
+        # Get the time off of the internet.
+        # os.system("sudo ntpdate time-a-g.nist.gov time-b-g.nist.gov time-c-g.nist.gov time-d-g.nist.gov time-d-g.nist.gov time-e-g.nist.gov time-e-g.nist.gov time-a-wwv.nist.gov time-b-wwv.nist.gov time-c-wwv.nist.gov time-d-wwv.nist.gov time-d-wwv.nist.gov time-e-wwv.nist.gov time-e-wwv.nist.gov time-a-b.nist.gov time-b-b.nist.gov time-c-b.nist.gov time-d-b.nist.gov time-d-b.nist.gov time-e-b.nist.gov time-e-b.nist.gov time.nist.gov utcnist.colorado.edu utcnist2.colorado.edu")
+        while loop_time_set > 0 and is_set == False:
+            try:
+                os.system("sudo systemctl restart systemd-timesyncd")
+                is_set = True
+            except:
+                print("couldn't update time from systemctl command")
+            try:
+                os.system("sudo ntpdate -q 0.us.pool.ntp.org")
+                is_set = True
+            except Exception as ex:
+                print("couldn't find time")
+            # Set the hardware clock
+            try:
+                os.system("sudo hwclock -w")
+                is_set = True
+            except Exception as ex:
+                print("couldn't hwclock -w")
+
+            try:
+                os.system("sudo hwclock -s")
+                is_set = True
+            except Exception as ex:
+                print("couldn't hwclock -s")
+            time.sleep(5)
+            loop_time_set -= 1
+        return
+
 def main():
     piCam = webcam()
     print(piCam.file_date_string())
