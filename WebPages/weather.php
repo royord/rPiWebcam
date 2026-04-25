b6adf696622fb7f33d6e998fcfe3ef05f4961eb6
royord <royord@royord.com>
2026-04-24 19:29:43 -0700

    weather page

---
+++ WebPages/weather.php
@@ -0,0 +1,18 @@
+<div class="container-fluid d-flex justify-content-center">
+<h3 class="widget-title">
+    Weather Forecast
+</h3>
+</div>
+<div class="container-fluid d-flex justify-content-center">
+<!-- Weather
+    Weather was included as an image so that it would scale well for the dynamic site.
+-->
+</a>
+</div>
+<div class="container-fluid d-flex justify-content-center">
+<br>
+<a href="https://forecast.weather.gov/MapClick.php?lon=-108.64918518904595&lat=32.45853442739431" target="_blank">
+    CLICK HERE for Additional Weather from NOAA (Contains More Detail)
+</a>
+</div>
+<!-- End Weather -->
