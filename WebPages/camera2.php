4ec433cfacbde7b293f0c63fbe0856908d460561
royord <royord@royord.com>
2026-04-24 19:13:49 -0700

    camera2

---
+++ WebPages/camera2.php
@@ -0,0 +1,7 @@
+<section id="custom_html-2" class="widget_text widget widget-1 even widget-first widget_custom_html">
+    <div class="widget_text widget-wrap">
+        <div class="textwidget custom-html-widget">
+            <img src="cameras/camera2/live.jpg" width="100%">
+        </div>
+    </div>
+</section>
