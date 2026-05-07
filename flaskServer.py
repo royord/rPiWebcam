63d10e6fbf48c1e343267e92b3926ebc2cc4e778
royord <royord@royord.com>
2026-05-07 01:02:51 -0700

    change quality

--- flaskServer.py
+++ flaskServer.py
@@ -701,7 +701,7 @@ if __name__ == '__main__':
 
     # Start recording to output
     # picam2.start_recording(JpegEncoder(q=85), FileOutput(output))
-    picam2.start_recording(JpegEncoder(q=100), FileOutput(output))
+    picam2.start_recording(JpegEncoder(q=60), FileOutput(output))
 
     # logging.basicConfig(level=logging.DEBUG)
 
